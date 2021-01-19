fac54eff51862eeb8916a6a5540fda12a0167e8c
Pedro Martin <pataruco@gmail.com>
2021-01-19 19:48:47 +0000

    Add a react lighbox gallery

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FsLightbox from 'fslightbox-react';
+
+import imageOne from '../assets/images/03.png';
+import imagetwo from '../assets/images/04.png';
+import imageThree from '../assets/images/05.png';
 
 const Gallery: React.FC = () => {
-  return <h1>Galley</h1>;
+  const [toggler, setToggler] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setToggler(!toggler)}>Toggle Lightbox</button>
+      <FsLightbox
+        toggler={toggler}
+        sources={[imageOne, imagetwo, imageThree]}
+      />
+    </>
+  );
 };
 
 export default Gallery;
