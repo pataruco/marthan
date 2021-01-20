a0ffdb4b75d4489f774a386da14e0e8b6c31a2ce
Pedro Martin <pataruco@gmail.com>
2021-01-20 09:16:50 +0000

    Rendering all images 🚀

---
+++ src/components/gallery/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GalleryItem = () => {
+  return (
+    <article>
+      <picture>
+        <img src="https://source.unsplash.com/random" alt="" />
+      </picture>
+    </article>
+  );
+};
+
+export default GalleryItem;
--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
 import FsLightbox from 'fslightbox-react';
-
-import imageOne from '../assets/images/03.png';
-import imagetwo from '../assets/images/04.png';
-import imageThree from '../assets/images/05.png';
+import imagesPaths from '../components/gallery/paths';
 
 const Gallery: React.FC = () => {
   const [toggler, setToggler] = useState(false);
 
   return (
-    <>
-      <button onClick={() => setToggler(!toggler)}>Toggle Lightbox</button>
-      <FsLightbox
-        toggler={toggler}
-        sources={[imageOne, imagetwo, imageThree]}
-      />
-    </>
+    <main>
+      {imagesPaths.map((imagePath, i) => (
+        <article key={i}>
+          <picture>
+            <img src={imagePath[250]} alt="" />
+          </picture>
+        </article>
+      ))}
+    </main>
   );
 };
 
