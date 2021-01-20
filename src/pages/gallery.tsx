9dcf6c641a0f3ea0ffb1a30b675ccd5ab3c1b9c1
Pedro Martin <pataruco@gmail.com>
2021-01-20 11:35:03 +0000

    Add gallery

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -2,8 +2,17 @@ import React, { useState } from 'react';
 import FsLightbox from 'fslightbox-react';
 import imagesPaths from '../components/gallery/paths';
 
+const imagesPaths800 = imagesPaths.map((path) => path[800]);
+
 const Gallery: React.FC = () => {
   const [toggler, setToggler] = useState(false);
+  const [productIndex, setProductIndex] = useState(0);
+
+  const handleClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setToggler(!toggler);
+    // setProductIndex();
+  };
 
   return (
     <main>
@@ -11,13 +20,21 @@ const Gallery: React.FC = () => {
       {imagesPaths.map((imagePath, i) => {
         console.log({ imagePath });
         return (
-          <article key={i}>
+          <article key={i} onClick={handleClick}>
             <picture>
               <img src={imagePath[250]} alt="" />
             </picture>
           </article>
         );
       })}
+
+      <button onClick={() => setToggler(!toggler)}>Toggle Lightbox</button>
+      <button onClick={() => setProductIndex(1)}>Load second product</button>
+      <FsLightbox
+        toggler={toggler}
+        sources={imagesPaths800}
+        key={productIndex}
+      />
     </main>
   );
 };
