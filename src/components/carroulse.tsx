f26658878f99fa54e82359cfbcc19483e8529c06
Pedro Martin <pataruco@gmail.com>
2021-01-21 19:21:13 +0000

    Draft nav header

---
+++ src/components/carroulse.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import Carousel from 'react-alice-carousel';
+import { Props } from 'react-alice-carousel/lib/types';
+import paths from '../lib/paths';
+
+const paths800 = paths.map((path) => path[800]);
+
+const handleDragStart = (e: React.MouseEvent) => e.preventDefault();
+
+const slides = paths800.map((path) => {
+  return (
+    <picture className="slide">
+      <img src={path} onDragStart={handleDragStart} alt="" />
+    </picture>
+  );
+});
+
+const StyledSection = styled.section`
+  min-height: 45vh;
+  background-color: rgb(0, 0, 0);
+
+  .carrousel-wrapper {
+    max-width: 50%;
+    margin: 0 auto;
+  }
+
+  .alice-carousel__stage-item {
+    width: 100% !important;
+  }
+
+  .slide {
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+const Carrousel: React.FC = () => {
+  return (
+    <StyledSection>
+      <div className="carrousel-wrapper">
+        <Carousel
+          animationDuration={10000}
+          animationType={'fadeout'}
+          autoPlay
+          autoPlayStrategy={'none'}
+          autoWidth
+          // autoHeight
+          disableButtonsControls
+          disableDotsControls
+          infinite
+          items={slides}
+          mouseTracking
+        />
+      </div>
+    </StyledSection>
+  );
+};
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,60 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-import paths from '../lib/paths';
-
-import Carousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import { Props } from 'react-alice-carousel/lib/types';
-
-const paths800 = paths.map((path) => path[800]);
-
-const handleDragStart = (e: React.MouseEvent) => e.preventDefault();
-
-const slides = paths800.map((path) => {
-  return (
-    <picture className="slide">
-      <img src={path} onDragStart={handleDragStart} alt="" />
-    </picture>
-  );
-});
 
 const StyledHeader = styled.header`
-  min-height: 45vh;
   background-color: rgb(0, 0, 0);
-
-  .carrousel-wrapper {
-    max-width: 50%;
-    margin: 0 auto;
-  }
-
-  .alice-carousel__stage-item {
-    width: 100% !important;
-  }
-
-  .slide {
-    width: 100%;
-    object-fit: cover;
+  padding: 1.25rem;
+  h1 {
+    color: white;
   }
 `;
 
 const Header: React.FC = () => {
   return (
     <StyledHeader>
-      <div className="carrousel-wrapper">
-        <Carousel
-          animationDuration={10000}
-          animationType={'fadeout'}
-          autoPlay
-          autoPlayStrategy={'none'}
-          autoWidth
-          // autoHeight
-          disableButtonsControls
-          disableDotsControls
-          infinite
-          items={slides}
-          mouseTracking
-        />
-      </div>
+      <h1>Pedro Marthan</h1>
     </StyledHeader>
   );
 };
