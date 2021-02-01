b6a720e3cd0dc8094265c93e6ea1500a1fd1ef42
Pedro Martin <pataruco@gmail.com>
2021-02-01 09:44:02 +0000

    remove content security policy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createBrowserHistory } from 'history';
 import { Switch, Route, Router } from 'react-router-dom';
 import Home from '../pages/home';
 import ErrorPage from '../pages/404';
 import App from '../components/App';
 import Gallery from '../pages/gallery';
+import ReactGA from 'react-ga';
 
 export const history = createBrowserHistory();
 
@@ -12,16 +13,25 @@ const CustomBrowserRouter: React.FC = ({ children }) => (
   <Router history={history}>{children}</Router>
 );
 
-const Routes = () => (
-  <App>
-    <CustomBrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/gallery" component={Gallery} />
-        <Route path="/*" component={ErrorPage} />
-      </Switch>
-    </CustomBrowserRouter>
-  </App>
-);
+const Routes = () => {
+  useEffect(() => {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV != 'development') {
+      ReactGA.initialize('G-T4T6Q95Z67');
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  }, []);
+
+  return (
+    <App>
+      <CustomBrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/*" component={ErrorPage} />
+        </Switch>
+      </CustomBrowserRouter>
+    </App>
+  );
+};
 
 export default Routes;
