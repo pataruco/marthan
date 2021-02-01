b7a19ca204856c96e440c20b7335b9f68543be57
Pedro Martin <pataruco@gmail.com>
2021-02-01 09:15:33 +0000

    Remove react GA and service worker

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import ReactGA from 'react-ga';
 import 'normalize.css';
 import '../styles/index.css';
-
-ReactGA.initialize('G-T4T6Q95Z67');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 const StyledDiv = styled.div`
   display: flex;
