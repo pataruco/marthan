91615f16b1298246a6d90e903a1ee6ea3c703fff
Pedro Martin <pataruco@gmail.com>
2025-12-26 16:23:25 +0000

    chore: add galeria page

---
+++ src/pages/galeria.ts
@@ -0,0 +1,38 @@
+import { paths } from "../shared/paths";
+
+const galleryWrapper = document.querySelector(".gallery");
+
+const renderGallery = () => {
+	let gallery = ``;
+	paths.forEach(({ imgSrc, footnote, year }, index) => {
+		gallery += `
+		<a data-slide-number="${index}"
+		  data-type="image"
+			data-fslightbox="gallery"
+			href="${imgSrc}"
+			data-caption="${footnote ? footnote.join(", ") : ""}"
+		>
+		  <picture>
+				<img src="${imgSrc}" alt="${footnote ? footnote.join(", ") : ""}" loading="lazy">
+		  </picture>
+		  <footer>
+				<p>
+				 ${year ? `<time datetime="${year}">${year}</time>` : "Fecha desconocida"}
+				</p>
+				${footnote ? footnote.map((item) => `<p>${item}</p>`).join("") : ""}
+		  </footer>
+		</a>
+    `;
+	});
+
+	if (galleryWrapper) {
+		galleryWrapper.innerHTML = gallery;
+	}
+
+	// Refresh FSLightbox to recognize new images
+	if (typeof refreshFsLightbox !== "undefined") {
+		refreshFsLightbox();
+	}
+};
+
+document.addEventListener("DOMContentLoaded", renderGallery);
