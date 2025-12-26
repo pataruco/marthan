90f8aa9c35d68a1d6970b146b24f4f2bf5db25bd
Pedro Martin <pataruco@gmail.com>
2025-12-26 11:09:47 +0000

    chore: render gallery

---
+++ src/paths.ts
@@ -0,0 +1,598 @@
+const BASE_URL = "https://storage.googleapis.com/pataruco/fotos_papa";
+
+export const paths = [
+	{
+		imgSrc: `${BASE_URL}/05.JPG`,
+		footnote: ["Premio mejor primer actor", "Círculo de críticos de teatro"],
+		year: 1978,
+	},
+	{
+		imgSrc: `${BASE_URL}/06.JPG`,
+		footnote: ["Las Pantuflas.", "Director: Romeo Costea"],
+		year: 1983,
+	},
+	{
+		imgSrc: `${BASE_URL}/08.JPG`,
+		footnote: null,
+		year: 1956,
+	},
+	{
+		imgSrc: `${BASE_URL}/09.JPG`,
+		footnote: ["El rincón del malo.", " Autor: Gilberto Pinto"],
+		year: 1956,
+	},
+	{
+		imgSrc: `${BASE_URL}/10.JPG`,
+		footnote: ["El extraño viaje de Simón el malo"],
+		year: 1970,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/12.JPG`,
+		footnote: ["El tirano Aguirre"],
+		year: 1956,
+	},
+	{
+		imgSrc: `${BASE_URL}/13.JPG`,
+		footnote: ["Cantaclaro"],
+		year: 1957,
+	},
+	{
+		imgSrc: `${BASE_URL}/14.JPG`,
+		footnote: ["1er. aniversario de Televisa"],
+		year: 1953,
+	},
+	{
+		imgSrc: `${BASE_URL}/15.JPG`,
+		footnote: [
+			"Juego de niños, de Ruiz Iriarte.",
+			"Programa: Teatro del hogar",
+			"Una escena con Aldo Monti, Ciro Medina, Liliana Durán y Reyna Hidalgo",
+		],
+		year: 1956,
+	},
+	{
+		imgSrc: `${BASE_URL}/16.JPG`,
+		footnote: ["Shubert", "Programa especial para TV"],
+		year: 1956,
+	},
+	{
+		imgSrc: `${BASE_URL}/17.JPG`,
+		footnote: ["Telenovela en Televisa"],
+		year: 1957,
+	},
+	{
+		imgSrc: `${BASE_URL}/18.JPG`,
+		footnote: ["Piar de Ali Lasser", "Dirección: Romeo Costea"],
+		year: 1975,
+	},
+	{
+		imgSrc: `${BASE_URL}/19.JPG`,
+		footnote: [
+			"Dulce pajaro de juventud de Tenesse Williams",
+			"Dirección: Romeo Costea",
+		],
+		year: 1988,
+	},
+	{
+		imgSrc: `${BASE_URL}/20.JPG`,
+		footnote: ["La Mandrágora de Nicólas Maquiavelo"],
+		year: 1965,
+	},
+	{
+		imgSrc: `${BASE_URL}/21.JPG`,
+		footnote: [
+			"Todos los días son sábados",
+			"Dirección: Enver Cordido",
+			"Guión: Salvador Garmendia",
+		],
+		year: 1977,
+	},
+	{
+		imgSrc: `${BASE_URL}/22.JPG`,
+		footnote: ["Teatro del hogar", "Televisa"],
+		year: 1958,
+	},
+	{
+		imgSrc: `${BASE_URL}/23.JPG`,
+		footnote: ["Las Pantuflas.", "Director: Romeo Costea"],
+		year: 1983,
+	},
+	{
+		imgSrc: `${BASE_URL}/24.JPG`,
+		footnote: ["Telenovela", "Televisa"],
+		year: 1958,
+	},
+	{
+		imgSrc: `${BASE_URL}/25.JPG`,
+		footnote: ["Teatro del hogar", "Televisa"],
+		year: 1958,
+	},
+	{
+		imgSrc: `${BASE_URL}/26.JPG`,
+		footnote: ["Teatro del hogar", "Televisa"],
+		year: 1958,
+	},
+	{
+		imgSrc: `${BASE_URL}/27.JPG`,
+		footnote: ["Ha llegado un inspector de John Boynton Priestley"],
+		year: 1960,
+	},
+	{
+		imgSrc: `${BASE_URL}/28.JPG`,
+		footnote: null,
+		year: 1948,
+	},
+	{
+		imgSrc: `${BASE_URL}/29.JPG`,
+		footnote: null,
+		year: 1958,
+	},
+	{
+		imgSrc: `${BASE_URL}/30.JPG`,
+		footnote: [
+			"Todos los días son sábados",
+			"Dirección: Enver Cordido",
+			"Guión: Salvador Garmendia",
+		],
+		year: 1977,
+	},
+	{
+		imgSrc: `${BASE_URL}/31.JPG`,
+		footnote: [
+			"Trampa para un hombre solo de Robert Thomas",
+			"Dirección: Gilberto Pinto",
+			"Bárbara Teyde en la foto",
+		],
+		year: 1975,
+	},
+	{
+		imgSrc: `${BASE_URL}/32.JPG`,
+		footnote: ["El extraño viaje de Simón el malo"],
+		year: 1970,
+	},
+	{
+		imgSrc: `${BASE_URL}/33.JPG`,
+		footnote: [
+			"Madame Pompinette",
+			"Dirección: Isaac Chocrón",
+			"Guión: José Gabriel Nuñez",
+		],
+		year: 1978,
+	},
+	{
+		imgSrc: `${BASE_URL}/34.JPG`,
+		footnote: ["El extraño viaje de Simón el malo", "Con Eva Blanco"],
+		year: 1970,
+	},
+	{
+		imgSrc: `${BASE_URL}/35.JPG`,
+		footnote: [
+			"Madame Pompinette",
+			"Dirección: Isaac Chocrón",
+			"Guión: José Gabriel Nuñez",
+		],
+		year: 1978,
+	},
+	{
+		imgSrc: `${BASE_URL}/36.JPG`,
+		footnote: [
+			"Todos los días son sábados",
+			"Dirección: Enver Cordido",
+			"Guión: Salvador Garmendia",
+		],
+		year: 1977,
+	},
+	{
+		imgSrc: `${BASE_URL}/37.JPG`,
+		footnote: [
+			"Todos los días son sábados",
+			"Dirección: Enver Cordido",
+			"Guión: Salvador Garmendia",
+		],
+		year: 1977,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/42.JPG`,
+		footnote: [
+			"Pedido de mano de Anton Chejov",
+			"Primera obra de teatro donde actuó",
+		],
+		year: 1948,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/48.JPG`,
+		footnote: [
+			"Todos los días son sábados",
+			"Dirección: Enver Cordido",
+			"Guión: Salvador Garmendia",
+		],
+		year: 1977,
+	},
+	{
+		imgSrc: `${BASE_URL}/49.JPG`,
+		footnote: [
+			"Madame Pompinette",
+			"Dirección: Isaac Chocrón",
+			"Guión: José Gabriel Nuñez",
+		],
+		year: 1978,
+	},
+	{
+		imgSrc: `${BASE_URL}/50.JPG`,
+		footnote: [
+			"Madame Pompinette",
+			"Dirección: Isaac Chocrón",
+			"Guión: José Gabriel Nuñez",
+		],
+		year: 1978,
+	},
+	{
+		imgSrc: `${BASE_URL}/51.JPG`,
+		footnote: [
+			"Madame Pompinette",
+			"Dirección: Isaac Chocrón",
+			"Guión: José Gabriel Nuñez",
+		],
+		year: 1978,
+	},
+	{
+		imgSrc: `${BASE_URL}/52.JPG`,
+		footnote: [
+			"Madame Pompinette",
+			"Dirección: Isaac Chocrón",
+			"Guión: José Gabriel Nuñez",
+		],
+		year: 1978,
+	},
+	{
+		imgSrc: `${BASE_URL}/53.JPG`,
+		footnote: [
+			"Madame Pompinette",
+			"Dirección: Isaac Chocrón",
+			"Guión: José Gabriel Nuñez",
+		],
+		year: 1978,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/55.JPG`,
+		footnote: [
+			"Trampa para un hombre solo de Robert Thomas",
+			"Dirección: Gilberto Pinto",
+			"Bárbara Teyde en la foto",
+		],
+		year: 1975,
+	},
+	{
+		imgSrc: `${BASE_URL}/56.JPG`,
+		footnote: [
+			"Trampa para un hombre solo de Robert Thomas",
+			"Dirección: Gilberto Pinto",
+			"Bárbara Teyde en la foto",
+		],
+		year: 1975,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/58.JPG`,
+		footnote: ["Programa: Teatro del hogar"],
+		year: 1956,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/60.JPG`,
+		footnote: [
+			"Dulce pajaro de juventud de Tenesse Williams",
+			"Dirección: Romeo Costea",
+		],
+		year: 1988,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/63.JPG`,
+		footnote: [
+			"Dulce pajaro de juventud de Tenesse Williams",
+			"Dirección: Romeo Costea",
+		],
+		year: 1988,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/77.jpg`,
+		footnote: [
+			"Trampa para un hombre solo de Robert Thomas",
+			"Dirección: Gilberto Pinto",
+			"Bárbara Teyde en la foto",
+		],
+		year: 1975,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/87.JPG`,
+		footnote: null,
+		year: 1946,
+	},
+	{
+		imgSrc: `${BASE_URL}/88.JPG`,
+		footnote: null,
+		year: 1958,
+	},
+	{
+		imgSrc: `${BASE_URL}/89.JPG`,
+		footnote: [
+			"Pedido de mano de Anton Chejov",
+			"Primera obra de teatro donde actuó",
+		],
+		year: 1948,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/93.JPG`,
+		footnote: [
+			"Trampa para un hombre solo de Robert Thomas",
+			"Dirección: Gilberto Pinto",
+			"Bárbara Teyde en la foto",
+		],
+		year: 1975,
+	},
+	{
+		imgSrc: `${BASE_URL}/94.JPG`,
+		footnote: [
+			"Todos los días son sábados",
+			"Dirección: Enver Cordido",
+			"Guión: Salvador Garmendia",
+		],
+		year: 1977,
+	},
+	{
+		imgSrc: `${BASE_URL}/95.JPG`,
+		footnote: [
+			"Todos los días son sábados",
+			"Dirección: Enver Cordido",
+			"Guión: Salvador Garmendia",
+		],
+		year: 1977,
+	},
+	{
+		imgSrc: `${BASE_URL}/96.JPG`,
+		footnote: [
+			"Dulce pajaro de juventud de Tenesse Williams",
+			"Dirección: Romeo Costea",
+		],
+		year: 1988,
+	},
+	{
+		imgSrc: `${BASE_URL}/97.JPG`,
+		footnote: ["El tirano Aguirre"],
+		year: 1956,
+	},
+	{
+		imgSrc: `${BASE_URL}/98.JPG`,
+		footnote: null,
+		year: 1980,
+	},
+	{
+		imgSrc: `${BASE_URL}/99.JPG`,
+		footnote: [
+			"Trampa para un hombre solo de Robert Thomas",
+			"Dirección: Gilberto Pinto",
+			"Bárbara Teyde en la foto",
+		],
+		year: 1975,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/101.JPG`,
+		footnote: [
+			"Pedido de mano de Anton Chejov",
+			"Primera obra de teatro donde actuó",
+		],
+		year: 1948,
+	},
+
+	{
+		imgSrc: `${BASE_URL}/04.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/07.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/11.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/38.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/39.JPG`,
+		footnote: ["Telenovela en Televisa"],
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/40.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/41.JPG`,
+		footnote: ["Telenovela en Televisa"],
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/43.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/44.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/45.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/46.JPG`,
+		footnote: ["Telenovela en Televisa"],
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/47.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/54.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/57.JPG`,
+		footnote: ["George Dandin o el marido confundido de Molière"],
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/59.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/61.JPG`,
+		footnote: ["Con Aura Rivas"],
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/62.JPG`,
+		footnote: ["Con Eva Blanco"],
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/64.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/65.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/66.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/68.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/69.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/70.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/71.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/72.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/73.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/75.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/78.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/79.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/80.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/81.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/82.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/83.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/84.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/85.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/86.jpg`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/90.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/91.JPG`,
+		footnote: ["INCIBA"],
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/92.JPG`,
+		footnote: null,
+		year: null,
+	},
+	{
+		imgSrc: `${BASE_URL}/100.jpg`,
+		footnote: null,
+		year: null,
+	},
+];
+
+export default paths;
