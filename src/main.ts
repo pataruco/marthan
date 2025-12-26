c5b59beaa30091218de49c44ad7462c012dd2cc7
Pedro Martin <pataruco@gmail.com>
2025-12-26 11:09:47 +0000

    chore: render gallery

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,28 @@
 import "./styles/index.css";
-console.log("Hello, World!");
+import { paths } from "./paths";
+
+const galleryWrapper = document.querySelector(".gallery");
+
+const renderGallery = () => {
+	let gallery = ``;
+
+	console.log({ paths });
+	paths.forEach(({ imgSrc, footnote, year }) => {
+		gallery += `
+      <a data-fslightbox="gallery" href="${imgSrc}" data-type="image">
+        <img src="${imgSrc}" alt="${footnote ? footnote.join(", ") : ""}">
+      </a>
+    `;
+	});
+
+	if (galleryWrapper) {
+		galleryWrapper.innerHTML = gallery;
+	}
+
+	// Refresh FSLightbox to recognize new images
+	if (typeof refreshFsLightbox !== "undefined") {
+		refreshFsLightbox();
+	}
+};
+
+document.addEventListener("DOMContentLoaded", renderGallery);
